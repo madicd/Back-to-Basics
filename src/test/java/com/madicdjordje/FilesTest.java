6de08c7f15f5add501acf86fe093f77b88fd7627
Djordje Madic <madicdjordje@gmail.com>
2017-11-19 19:29:52 +0100

    Creating files and directories

--- src/test/java/com/madicdjordje/FilesTest.java
+++ src/test/java/com/madicdjordje/FilesTest.java
@@ -3,10 +3,12 @@ package com.madicdjordje;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 import static java.nio.file.Files.*;
 import static java.nio.file.Paths.get;
+import static java.util.UUID.randomUUID;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -16,23 +18,23 @@ public class FilesTest {
 
     @Test
     public void testExists() {
-        Path homePath = get(HOME);
+        Path home = get(HOME);
 
-        assertTrue(exists(homePath));
+        assertTrue(exists(home));
     }
 
     @Test
     public void testNotExists() {
-        Path fakeFilePath = get(HOME + "/fake.txt");
+        Path fakeFile = get(HOME + "/fake.txt");
 
-        assertTrue(notExists(fakeFilePath));
+        assertTrue(notExists(fakeFile));
     }
 
     @Test
     public void testFileCheck() {
-        Path homePath = get(HOME);
+        Path home = get(HOME);
 
-        assertFalse(isRegularFile(homePath));
+        assertFalse(isRegularFile(home));
     }
 
     @Test
@@ -42,4 +44,56 @@ public class FilesTest {
 
         assertTrue(isSameFile(home1, home2));
     }
+
+    @Test
+    public void testCreateRegularFile() throws IOException {
+        String fileName = "regular_file_" + randomUUID() + ".txt";
+        Path file = get(HOME + "/" + fileName);
+
+        assertTrue(notExists(file));
+        createFile(file);
+        assertTrue(exists(file));
+        assertTrue(isRegularFile(file));
+
+        delete(file);
+    }
+
+    @Test
+    public void testCreateDir() throws IOException {
+        String dirName = "dir_" + randomUUID();
+        Path dir = get(HOME + "/" + dirName);
+
+        assertTrue(notExists(dir));
+        createDirectory(dir);
+        assertTrue(exists(dir));
+        assertTrue(isDirectory(dir));
+
+        delete(dir);
+    }
+
+    @Test(expected = NoSuchFileException.class)
+    public void testCreateSubdirFailsWhenDirDoesntExist() throws IOException {
+        String dirName = "dir_" + randomUUID();
+        Path dir = get(HOME + "/" + dirName);
+        Path subdir = dir.resolve("subdir");
+
+        assertTrue(notExists(subdir));
+        createDirectory(subdir);
+    }
+
+    @Test
+    public void testCreateDirectoriesRecursively() throws IOException {
+        String dirName = "dir_" + randomUUID();
+        Path dir = get(HOME + "/" + dirName);
+        Path subdir = dir.resolve("subdir");
+
+        assertTrue(notExists(dir));
+        createDirectories(subdir);
+        assertTrue(exists(dir));
+
+        delete(subdir);
+        delete(dir);
+    }
+
+
 }
